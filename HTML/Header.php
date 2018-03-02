6d7a7747f3c3fed3fba5331f49ae749fef3ea5f8
Pim-dijk <31619916+Pim-dijk@users.noreply.github.com>
2018-03-02 11:33:36 +0100

    Header and Footer Responsive + base CSS

---
+++ HTML/Header.php
@@ -0,0 +1,54 @@
+<div id="headerContainer">
+	<!--Logo-->
+	<div class="col-xs-12">
+	    <a href="#">
+			<img id="logo" src="images/Header/logo.png" class="">
+		</a>
+		
+		<!--Shopping Cart-->
+		<a id="shoppingCart" href="#" class="pull-right">
+			<img src="images/Header/shopping_cart.png" class="shoppingCart">
+			<!--Itemscount in cart-->
+			<p class="cartCount">2</p>
+		</a>
+	</div>
+
+	<!--Hamburger Menu-->
+	<div id="hamburger" href="#" class="hamburger">
+		<div class="line"></div>
+		<div class="line"></div>
+		<div class="line"></div>
+	</div>
+	<!--Nav section-->
+    <nav id="header" class="">
+		<ul>
+		  <a href="#">
+			<li>Aanbiedingen</li>
+		  </a> 
+		  <a href="#">
+			<li>Componenten</li>
+		  </a> 
+		  <a href="#">
+			<li>Software</li>
+		  </a> 
+		  <a href="#">
+			<li>Randapparatuur</li>
+		  </a> 
+		  <a href="#">
+			<li>Systemen</li>
+		  </a> 
+		  <a href="#">
+			<li>Service</li>
+		  </a> 
+		  <a href="#">
+			<li>Contact</li>
+		  </a>
+		  <a href="#">
+			<li class="loginButton pull-right">Login</li>
+		  </a>
+		</ul>
+	<!--/nav - header--> 
+	</nav>
+	
+<!--/headerContainer-->
+</div>
---
+++ HTML/Index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PC4U</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+	<!--Stylesheet-->
+	<link type="text/css" rel="stylesheet" href="css/opmaak.css">
+	<link type="text/css" rel="stylesheet" href="css/Header.css">
+	<link type="text/css" rel="stylesheet" href="css/Footer.css">
+
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<script src="js/respond.js"></script>
+</head>
+
+<body>
+	<div id="container" class=container-fluid>
+		<?php include 'Header.php'; ?>
+		<?php include 'Footer.php'; ?>
+		<!--/container-->
+	</div>
+	
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="js/hamburger.js"></script>
+</body>
+	
+</html>
