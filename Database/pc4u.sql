4512fe960bf71f86f832cf4b911c51501dc5709e
Pim-dijk <31619916+Pim-dijk@users.noreply.github.com>
2018-03-16 11:18:23 +0100

    Added database
    Preliminary database with customer, products and customers

---
+++ Database/pc4u.sql
@@ -0,0 +1,135 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 16, 2018 at 11:18 AM
+-- Server version: 10.1.24-MariaDB
+-- PHP Version: 7.0.20
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pc4u`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `CategoryID` int(11) NOT NULL,
+  `Category` varchar(48) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`CategoryID`, `Category`) VALUES
+(1, 'Processor');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `CustomerID` int(10) NOT NULL,
+  `Email` varchar(128) NOT NULL,
+  `Password` varchar(256) NOT NULL,
+  `PhoneNumber` int(15) NOT NULL,
+  `Street` varchar(48) NOT NULL,
+  `Zipcode` varchar(10) NOT NULL,
+  `HouseNumber` int(3) NOT NULL,
+  `HNAddition` varchar(5) NOT NULL,
+  `City` varchar(48) NOT NULL,
+  `Country` varchar(48) NOT NULL,
+  `Private` int(1) NOT NULL,
+  `Firstname` varchar(48) NOT NULL,
+  `Lastname` varchar(64) NOT NULL,
+  `DOB` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `ID` int(8) NOT NULL,
+  `CategoryID` int(8) DEFAULT NULL,
+  `ArtNumber` int(8) NOT NULL,
+  `ArtName` varchar(40) NOT NULL,
+  `Description` text NOT NULL,
+  `Price` decimal(10,0) NOT NULL,
+  `Availability` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`ID`, `CategoryID`, `ArtNumber`, `ArtName`, `Description`, `Price`, `Availability`) VALUES
+(5, NULL, 546, 'bhsfja', '', '0', 0),
+(156, 0, 16516, 'hsagjhasjsa', '', '0', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`CategoryID`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`CustomerID`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `categoryID` (`CategoryID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `CategoryID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `CustomerID` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `ID` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=157;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
